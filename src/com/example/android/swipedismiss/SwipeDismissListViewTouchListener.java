f30a1755289f2c27df5721249a31cf1c007352bd
Jake Wharton <jw@squareup.com>
2012-06-24 01:40:03 -0700

    Convert Roman's sample into a full, working project.

--- src/com/example/android/swipedismiss/SwipeDismissListViewTouchListener.java
+++ src/com/example/android/swipedismiss/SwipeDismissListViewTouchListener.java
@@ -22,7 +22,6 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.graphics.Rect;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
 import android.view.View;
